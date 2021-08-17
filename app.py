2d68fab0c909c0fd1f0bb5553cf7a86c6cf06cd4
1paragraph <1paragraph@github.com>
2021-08-17 09:22:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ import streamlit as st
 uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg'])
 if uploaded_file is not None:
     file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type,"FileSize":uploaded_file.size}
-     st.write(file_details)
+    st.write(file_details)

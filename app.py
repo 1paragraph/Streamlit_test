0f9f96cf10c5f30553ebdba0aa79d0d8024f76bf
1paragraph <1paragraph@github.com>
2021-08-17 09:11:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,10 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-# Add a selectbox to the sidebar:
-add_selectbox = st.sidebar.selectbox(
-    'How would you like to be contacted?',
-    ('Email', 'Home phone', 'Mobile phone')
-)
 
-# Add a slider to the sidebar:
-add_slider = st.sidebar.slider(
-    'Select a range of values',
-    0.0, 100.0, (25.0, 75.0)
-)
+st.title('ВОУ!')
+st.text('Привет, Ваня, вот создал какую-то хуету, может быть мы сможем использовать это для тестирования приложений или развертки приложений.')
+
+data = np.array([1,2,3])
+
+st.bar_chart(data)

7650ef39db8643be706c3ae15216b973a4b38bf1
1paragraph <1paragraph@github.com>
2021-08-17 10:10:02 +0300

    adding some shit

--- app.py
+++ app.py
@@ -8,3 +8,7 @@ uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg'])
 if uploaded_file is not None:
     file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type,"FileSize":uploaded_file.size}
     st.write(file_details)
+
+a = cv2.imread(uploaded_file)
+
+st.write(file_details)

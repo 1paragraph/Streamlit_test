e90875e8e75892872bd4e98830444ca2331e64ba
1paragraph <1paragraph@github.com>
2021-08-17 09:24:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import opencv as cv2
 import streamlit as st
 
 
@@ -7,3 +8,5 @@ uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg'])
 if uploaded_file is not None:
     file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type,"FileSize":uploaded_file.size}
     st.write(file_details)
+
+print(uploaded_file)

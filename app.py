af993c6ef00595f7952ce79488675ea21c6304a4
1paragraph <1paragraph@github.com>
2021-08-17 09:06:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-import streamlit as st
-# To make things easier later, we're also importing numpy and pandas for
-# working with sample data.
 import numpy as np
 import pandas as pd
 
-streamlit run app.py
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+st.table(dataframe)
